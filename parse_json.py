41a8672bdc32d83e5b4076da7e1a1546f9027a70
Nailya <nailasarmurzanova7@gmail.com>
2025-02-16 19:59:45 +0600

    Added JSON parsing script

--- parse_json.py
+++ parse_json.py
@@ -1,1 +1,16 @@
+import json
 
+
+with open("sample-data.json", "r") as file:
+    data = json.load(file)
+
+
+print("Interface Status\n")
+print("{:<50} {:<10} {:<10} {:<10}".format("DN", "Description", "Speed", "MTU"))
+print("="*80)
+
+
+for interface in data["interfaces"]:
+    print("{:<50} {:<10} {:<10} {:<10}".format(
+        interface["dn"], interface["description"], interface["speed"], interface["mtu"]
+    ))
